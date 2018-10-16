adcc7f7b5f6e5f93b9a27483644c83649d9eac1e
Iro Enezli <enezliro@gmail.com>
2018-10-16 12:52:57 +0300

    first commit

---
+++ src/components/BoardRow.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Square from './Square';
+
+const BoardRow = ({ squares, from, onClick }) => (
+  <div /*className="board-row"*/>
+    {squares.map((square, i) => (
+      <Square onClick={()=>onClick(from+i)} value={square} />
+    ))}
+  </div>
+)
+
+export default BoardRow;
